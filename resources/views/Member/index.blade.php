592cfe6b84521f7345b2ac48ce74d8b9178359e0
= <=>
2025-10-08 13:22:26 +0700

    Membuat Dashboard

---
+++ resources/views/Member/index.blade.php
@@ -0,0 +1,7 @@
+@include('templates.navigation')
+<div class="container-fluid">
+  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Member</h1>
+  </div>
+</div>
+@include('templates.footer')
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\PeminjamanController;
+use App\Http\Controllers\AboutController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/Kategori', [KategoriController::class, 'index'])->name('Kategori');
+Route::get('/Buku', [BukuController::class, 'index'])->name('Buku');
+Route::get('/Member', [MemberController::class, 'index'])->name('Member');
+Route::get('/Peminjaman', [PeminjamanController::class, 'index'])->name('Peminjaman');
+Route::get('/About', [AboutController::class, 'index'])->name('About');
